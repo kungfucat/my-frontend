be1d3a6f4ca70eb3ac724670f10f804d3c461325
Harsh Bhardwaj <harshbhardwaj2357@gmail.com>
2021-01-31 17:17:54 +0530

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,60 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {useState} from "react";
+import Axios from "axios";
+import axios from "axios";
+
+const FileDownload = require('js-file-download');
 
 export default function Home() {
+
+  //React hooks to get the current text block
+  const [textBlock, setTextBlock] = useState("");
+  const [disableInputs, setDisableInputs] = useState(false);
+
+  const downloadFile = async () => {
+    axios.get("http://localhost:8000/reports", {params: {textBlock: textBlock}, responseType:'blob'}).then(function (response){
+      FileDownload(response.data, 'report.docx');
+    });
+  }
+  const submitHandler = (e) => {
+
+    if (disableInputs) {
+      return;
+    }
+    //do not refresh
+    e.preventDefault();
+    console.log(textBlock);
+    setDisableInputs(true);
+    downloadFile().then(r => setDisableInputs(false));
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className={styles.container}>
+        <Head>
+          <title>Word Doc Converter</title>
+          <link rel="icon" href="/favicon.ico"/>
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Convert text to a Word Document!
+          </h1>
+          <textarea name = "textBlock" id = "textBlock" className={styles.textBlock} disabled= {disableInputs} onChange={(e) => setTextBlock(e.target.value)}/>
+          <button id="submitButton" className={styles.submitButton} onClick={submitHandler} disabled={disableInputs}>
+            {disableInputs? "Please wait": "Submit"}</button>
+        </main>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
+        <footer className={styles.footer}>
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+              href="https://hb11.me/#home"
+              target="_blank"
+              rel="noopener noreferrer">
+            Created by Harsh Bhardwaj
           </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        </footer>
+      </div>
   )
 }
+
+//npm run dev
